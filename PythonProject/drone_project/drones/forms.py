4f8f239478842d66bb4f535b73885d5c23bae7dd
ggbond-1-1 <1910689906@qq.com>
2025-05-12 22:41:23 +0800

    Delete PythonProject/drone_project directory

--- PythonProject/drone_project/drones/forms.py
+++
@@ -1,8 +0,0 @@
-# drones/forms.py
-from django import forms
-from .models import Drone
-
-class DroneForm(forms.ModelForm):
-    class Meta:
-        model = Drone
-        fields = '__all__'  # 或者指定具体的字段
--- PythonProject/drone_project/goods/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Goods
-
-admin.site.register(Goods)
